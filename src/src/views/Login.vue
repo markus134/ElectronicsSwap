2c1c93d1d0e987eb47461c7885abc2a5d69c207d
Markus Veerbaum <mveerb@taltech.ee>
2023-10-21 22:18:45 +0300

    Make page look more similar to the prototype

--- src/src/views/Login.vue
+++ src/src/views/Login.vue
@@ -5,20 +5,20 @@
         <p class="text-base mb-6 text-lg">Sisselogimine</p>
         <form @submit.prevent="login">
           <div class="mb-6">
-            <label for="username" class="block text-sm font-medium text-gray-600 m-3 text-base">Nimi</label>
-            <input type="text" id="username" v-model="username" placeholder="Kirjuta oma nimi" class="w-full p-2 border rounded-md">
+            <label for="username" class="block font-medium mb-3 text-base">Nimi</label>
+            <input type="text" id="username" v-model="username" placeholder="Kirjuta oma nimi" class="w-full p-2 border bg-neutral-200">
           </div>
           <div class="mb-6">
-            <label for="password" class="block text-sm font-medium text-gray-600 m-3 text-base">Parool</label>
-            <input type="password" id="password" v-model="password" placeholder="Kirjuta oma parool" class="w-full p-2 border rounded-md">
+            <label for="password" class="block font-medium mb-3 text-base">Parool</label>
+            <input type="password" id="password" v-model="password" placeholder="Kirjuta oma parool" class="w-full p-2 border bg-neutral-200">
           </div>
           <div class="m-4 text-right">
             <a href="/forgot_password" class="font-thin">Unustasid oma parooli?</a>
           </div>
-          <div class="flex items-center justify-center mb-4">
-            <button type="submit" class="button-background text-white py-2 px-4 w-full rounded-md">Logi sisse</button>
+          <div class="flex items-center justify-center mb-8">
+            <button type="submit" class="button-background text-white py-2 px-4 w-full">Logi sisse</button>
           </div>
-          <div class="text-center">
+          <div class="text-center pb-4">
             <p>Kas pole veel kontot? <a href="/registration" class="font-medium">Registreeri</a></p>
           </div>
         </form>
