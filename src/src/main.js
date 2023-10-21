db16f914c31a6814050c1ff54d14713cbe96759b
Markus Veerbaum <mveerb@taltech.ee>
2023-10-21 21:50:28 +0300

    Add login page

--- src/src/main.js
+++ src/src/main.js
@@ -4,4 +4,7 @@ import '@/style/style.scss';
 import router from '@/router';
 import store from '@/store';
 
-createApp(App).use(router).use(store).mount('#app');
+createApp(App)
+  .use(router)
+  .use(store)
+  .mount('#app');
--- src/src/router/index.js
+++ src/src/router/index.js
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LandingPage from '@/views/LandingPage.vue';
+import Login from '@/views/Login.vue'; // Import the Login component
 
-const routes = [{ name: 'landing-page', path: '/', component: LandingPage }];
+const routes = [
+  { name: 'landing-page', path: '/', component: LandingPage },
+  { name: 'login', path: '/login', component: Login }, // Add a new route for the login page
+];
 
 const router = createRouter({
   history: createWebHistory(),
