a01214e8f7935c55558b4553905d2f22600998b0
Markus Veerbaum <mveerb@taltech.ee>
2023-10-22 00:06:07 +0300

    change style

--- src/src/views/Login.vue
+++ src/src/views/Login.vue
@@ -1,22 +1,22 @@
 <template>
-    <div class="min-h-screen flex items-center justify-center bg-neutral-200">
-      <div class="bg-white p-8 rounded shadow-md w-2/4 h-4/5 min-h-full">
+    <div class="min-h-screen flex items-center justify-center background">
+      <div class="bg-white p-8 rounded shadow-2xl w-2/4 h-4/5 min-h-full">
         <h1 class="text-2xl font-semibold mb-4">ElectronicsSwap</h1>
         <p class="text-base mb-6 text-lg">Sisselogimine</p>
         <form @submit.prevent="login">
           <div class="mb-6">
             <label for="username" class="block font-medium mb-3 text-base">Nimi</label>
-            <input type="text" id="username" v-model="username" placeholder="Kirjuta oma nimi" class="w-full p-2 border bg-neutral-200">
+            <input type="text" id="username" v-model="username" placeholder="Kirjuta oma nimi" class="w-full p-2 border-b-2 border-black hover:button-hover">
           </div>
           <div class="mb-6">
             <label for="password" class="block font-medium mb-3 text-base">Parool</label>
-            <input type="password" id="password" v-model="password" placeholder="Kirjuta oma parool" class="w-full p-2 border bg-neutral-200">
+            <input type="password" id="password" v-model="password" placeholder="Kirjuta oma parool" class="w-full p-2 border-b-2 border-black">
           </div>
           <div class="m-4 text-right">
             <a href="/forgot_password" class="font-thin">Unustasid oma parooli?</a>
           </div>
           <div class="flex items-center justify-center mb-8">
-            <button type="submit" class="button-background text-white py-2 px-4 w-full">Logi sisse</button>
+            <button type="submit" class="button-background text-white py-2 px-4 w-full rounded-md">Logi sisse</button>
           </div>
           <div class="text-center pb-4">
             <p>Kas pole veel kontot? <a href="/registration" class="font-medium">Registreeri</a></p>
@@ -45,7 +45,14 @@
 
 <style scoped>
 .button-background {
-    background-color: #484848;
+    background-color: #b4beef;
+}
+
+.button-hover {
+  border-color: #b4beef;
+}
+.background {
+  background-image: url("../assets/background.png");
 }
 </style>
   
