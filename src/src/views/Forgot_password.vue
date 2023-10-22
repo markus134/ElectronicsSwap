570f83ca0b14eec66208275cc6ced33c26e358fc
Markus Veerbaum <mveerb@taltech.ee>
2023-10-22 09:32:13 +0300

    change function name to better reflect what it's doing

--- src/src/views/Forgot_password.vue
+++ src/src/views/Forgot_password.vue
@@ -3,7 +3,7 @@
       <div class="bg-white p-8 rounded shadow-2xl w-2/4 h-4/5 min-h-full">
         <h1 class="text-2xl font-semibold mb-4">ElectronicsSwap</h1>
         <p class="text-base mb-6 text-lg">Parooli taastamine</p>
-        <form @submit.prevent="login">
+        <form @submit.prevent="forgot_password">
           
           <div class="mb-6">
             <input type="email" id="email" v-model="email" placeholder="Kirjuta oma email" class="transition ease-in-out delay-150 w-full p-2 border-b-2 border-black focus:outline-none focus:border-indigo-300 duration-300	">
@@ -26,7 +26,7 @@
       };
     },
     methods: {
-      login() {
+      forgot_password() {
         console.log("Email: " + this.email);
       }
     }
