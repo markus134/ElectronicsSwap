01d57f6e31492cfe9f400c0c1cf9b260d8da498d
Markus Veerbaum <mveerb@taltech.ee>
2023-10-22 09:22:50 +0300

    add forgot password page

--- src/src/router/index.js
+++ src/src/router/index.js
@@ -1,10 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LandingPage from '@/views/LandingPage.vue';
-import Registration from '@/views/Registration.vue'
-
+import Forgot_password from '@/views/Forgot_password.vue'
 const routes = [
   { name: 'landing-page', path: '/', component: LandingPage },
-  { name: 'registration', path: '/registration', component: Registration}
+  { name: 'forgot_password', path: '/forgot_password', component: Forgot_password}
 ];
 
 const router = createRouter({
---
+++ src/src/views/Forgot_password.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="min-h-screen flex items-center justify-center background">
+      <div class="bg-white p-8 rounded shadow-2xl w-2/4 h-4/5 min-h-full">
+        <h1 class="text-2xl font-semibold mb-4">ElectronicsSwap</h1>
+        <p class="text-base mb-6 text-lg">Parooli taastamine</p>
+        <form @submit.prevent="login">
+          
+          <div class="mb-6">
+            <input type="email" id="email" v-model="email" placeholder="Kirjuta oma email" class="transition ease-in-out delay-150 w-full p-2 border-b-2 border-black focus:outline-none focus:border-indigo-300 duration-300	">
+          </div>
+          
+          <div class="flex items-center justify-center mb-8">
+            <button type="submit" class="button-background text-white py-2 px-4 w-full rounded-md">Taasta</button>
+          </div>
+         
+        </form>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        email: ''
+      };
+    },
+    methods: {
+      login() {
+        console.log("Email: " + this.email);
+      }
+    }
+  };
+  </script>
+
+<style scoped>
+.button-background {
+    background-color: #b4beef;
+}
+
+.background {
+  background-image: url("../assets/background.png");
+}
+</style>
+  
