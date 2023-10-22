0d8a9ebcf21efea201a4c80806a02c57ac1a2926
Markus Veerbaum <mveerb@taltech.ee>
2023-10-22 08:35:38 +0300

    registration page

--- src/src/router/index.js
+++ src/src/router/index.js
@@ -1,11 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LandingPage from '@/views/LandingPage.vue';
-import Login from '@/views/Login.vue'; 
 import Registration from '@/views/Registration.vue'
 
 const routes = [
   { name: 'landing-page', path: '/', component: LandingPage },
-  { name: 'login', path: '/login', component: Login },
   { name: 'registration', path: '/registration', component: Registration}
 ];
 
--- src/src/views/Registration.vue
+++ src/src/views/Registration.vue
@@ -1,23 +1,26 @@
 <template>
-    <div class="min-h-screen flex items-center justify-center bg-neutral-200">
-      <div class="bg-white p-8 rounded shadow-md w-2/4 h-4/5 min-h-full">
+    <div class="min-h-screen flex items-center justify-center background">
+      <div class="bg-white p-8 rounded shadow-2xl w-2/4 h-4/5 min-h-full">
         <h1 class="text-2xl font-semibold mb-4">ElectronicsSwap</h1>
         <p class="text-base mb-6 text-lg">Registreerimine</p>
         <form @submit.prevent="register">
           <div class="mb-6">
             <label for="username" class="block font-medium mb-3 text-base">Nimi</label>
-            <input type="text" id="username" v-model="username" placeholder="Kirjuta oma nimi" class="w-full p-2 border bg-neutral-200">
+            <input type="text" id="username" v-model="username" placeholder="Kirjuta oma nimi" class="transition ease-in-out delay-150 w-full p-2 border-b-2 border-black focus:outline-none focus:border-indigo-300 duration-300">
           </div>
           <div class="mb-6">
             <label for="email" class="block font-medium mb-3 text-base">Email</label>
-            <input type="email" id="email" v-model="email" placeholder="Kirjuta oma email" class="w-full p-2 border bg-neutral-200">
+            <input type="email" id="email" v-model="email" placeholder="Kirjuta oma email" class="transition ease-in-out delay-150 w-full p-2 border-b-2 border-black focus:outline-none focus:border-indigo-300 duration-300">
           </div>
           <div class="mb-6">
             <label for="password" class="block font-medium mb-3 text-base">Parool</label>
-            <input type="password" id="password" v-model="password" placeholder="Kirjuta oma parool" class="w-full p-2 border bg-neutral-200">
+            <input type="password" id="password" v-model="password" placeholder="Kirjuta oma parool" class="transition ease-in-out delay-150 w-full p-2 border-b-2 border-black focus:outline-none focus:border-indigo-300 duration-300">
           </div>
           <div class="m-4 text-left">
-            <p class="font-thin">Olen nõus platvormi tingimustega</p>
+            <!--Default checkbox-->
+            <input type="checkbox" id="accept_toc" v-model="accept_toc" class="mr-4">
+            <label>Olen nõus platvormi tegevustega</label>
+           
           </div>
           <div class="flex items-center justify-center mb-8">
             <button type="submit" class="button-background text-white py-2 px-4 w-full">Registreeri</button>
@@ -36,7 +39,8 @@
       return {
         username: '',
         email: '',
-        password: ''
+        password: '',
+        accept_toc: false
       };
     },
     methods: {
@@ -44,14 +48,20 @@
         console.log("Username: " + this.username);
         console.log("Email: " + this.email)
         console.log("Password: " + this.password);
+        console.log("Accepts conditions: " + this.accept_toc)
       }
     }
   };
   </script>
+  
 
 <style scoped>
 .button-background {
-    background-color: #484848;
+    background-color: #b4beef;
+}
+
+.background {
+  background-image: url("../assets/background.png");
 }
 </style>
   
