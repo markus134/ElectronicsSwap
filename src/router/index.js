350cbd234aa2d14150a0d85ddec41131d1cd4e12
ppavli <ppavli@taltech.ee>
2023-10-25 17:20:00 +0300

    Adding the Tagasiside page to the Tagasiside branch.

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LandingPage from '@/views/LandingPage.vue';
-import Login from '@/views/Login.vue'; // Import the Login component
+import Login from '@/views/Login.vue';
+import Tagasiside from "@/views/Tagasiside.vue"; // Import the Login component
 
 const routes = [
   { name: 'landing-page', path: '/', component: LandingPage },
   { name: 'login', path: '/login', component: Login }, // Add a new route for the login page
+  {name: 'tagasiside', path: '/tagasiside', component: Tagasiside}
 ];
 
 const router = createRouter({
