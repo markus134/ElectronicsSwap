f9d55c031847a2ee3c5e539ed2785181479fe3bd
ppavli <ppavli@taltech.ee>
2023-10-28 18:15:32 +0300

    Creating Navbar component and adding it t Tagasiside.vue

--- src/views/Tagasiside.vue
+++ src/views/Tagasiside.vue
@@ -1,26 +1,6 @@
 <template>
   <div class="min-h-screen flex flex-col">
-    <!-- Navigation Bar -->
-    <div class="p-4">
-      <div class="flex justify-between items-center">
-        <!-- Bigger ElectronicsSwap button -->
-        <button class="nav-button" style="font-size: 1.5rem; padding: 1rem;">ElectronicsSwap</button>
-        <div class="flex space-x-4 items-center">
-          <button class="nav-button">Laenutamine</button>
-          <button class="nav-button">Vahetus</button>
-          <button class="nav-button">Loo pakkumine</button>
-          <button class="nav-button">Ostukorv</button>
-          <!-- Circular container for user picture -->
-          <div class="h-10 w-10 rounded-full overflow-hidden">
-            <!-- Placeholder for user picture or an actual image source -->
-            <img src="@/assets/man.jpg" alt="User Picture" class="h-full w-full object-cover" />
-          </div>
-          <button class="nav-button">Username</button>
-        </div>
-      </div>
-    </div>
-
-    <!-- Main Content -->
+    <Navbar />
     <div class="flex-grow flex items-center justify-center background mt-8">
       <div class="bg-white p-8 rounded shadow-2xl w-2/4">
         <h1 class="text-2xl font-semibold mb-4">ElectronicsSwap</h1>
@@ -35,7 +15,12 @@
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue";
+
 export default {
+  components: {
+    Navbar,
+  },
   data() {
     return {
       feedback: '',
@@ -44,7 +29,6 @@ export default {
   methods: {
     submitFeedback() {
       console.log("Feedback: " + this.feedback);
-      // Add logic to send feedback to your backend or perform any other actions
     },
   },
 };
@@ -57,17 +41,5 @@ export default {
 
 .background {
   background-image: url("../assets/background.png");
-}
-
-.nav-button {
-  background-color: transparent;
-  border: none;
-  color: #2d3748;
-  cursor: pointer;
-  margin-right: 10px; /* Adjust the margin as needed */
-}
-
-.nav-button:hover {
-  background-color: #e2e8f0; /* Light gray for hover effect */
 }
 </style>
