717f65a00cc5ed5871b0257e18f022920151a572
Markus Veerbaum <mveerb@taltech.ee>
2023-10-30 22:51:16 +0200

    rewrite code, use implicit refreshing and add some error checking

--- server/__init__.py
+++ server/__init__.py
@@ -1,22 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from extensions import jwt
+from flask_jwt_extended import JWTManager
+from datetime import timedelta
+import secrets
 
 db = SQLAlchemy()
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
 
-    # Configuration settings should be placed in a separate configuration file or environment variables
-    app.config['JWT_SECRET_KEY'] = 'your-secret-key'
+    app.config['JWT_SECRET_KEY'] = secrets.token_hex(32) # Secret key should be random
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@mysql-db/users'
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1) # JWT cookie should expire in an hour
     
     # Initialize the extensions within the application context
     db.init_app(app)
     jwt.init_app(app)
 
     # Import and register blueprints
-    from auth import auth
+    from auth import auth # This is here to avoid a circular import
+    
     app.register_blueprint(auth, url_prefix="/api/auth")
 
     return app
--- server/auth.py
+++ server/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from flask_jwt_extended import create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies, unset_jwt_cookies, jwt_required, jwt_refresh_token_required
+from flask_jwt_extended import create_access_token, set_access_cookies, unset_jwt_cookies, jwt_required
 from werkzeug.security import generate_password_hash
 from models import User 
 
@@ -17,10 +17,10 @@ def registration():
         if not username or not email or not password:
             return jsonify(message="Missing username, email or password"), 400
 
-        # Check if the user with the given username already exists
-        user = User.query.filter_by(username=username).first()
-        if user is not None:
-            return jsonify(message="User already exists."), 400
+        # Check if the user with the given username or email already exists
+        existing_user = User.query.filter((User.username == username) | (User.email == email)).first()
+        if existing_user is not None:
+            return jsonify(message="User with this username or email already exists."), 400
 
         user = User(
             username=username,
@@ -28,14 +28,13 @@ def registration():
             password=generate_password_hash(password)
         )
         user.add()
-
+        
         access_token = create_access_token(identity=user.id)
-        refresh_token = create_refresh_token(identity=user.id)
-
         response = jsonify()
         set_access_cookies(response, access_token)
-        set_refresh_cookies(response, refresh_token)
+        
         return response, 201
+    
     except Exception as e:
         return jsonify(message="An error occurred while processing your request." + str(e)), 500
 
@@ -53,19 +52,19 @@ def login():
 
         if user:
             access_token = create_access_token(identity=user.id)
-            refresh_token = create_refresh_token(identity=user.id)
-
             response = jsonify()
             set_access_cookies(response, access_token)
-            set_refresh_cookies(response, refresh_token)
+            
             return response, 201
+        
         else:
             return jsonify(message="Unauthorized"), 401
+        
     except Exception as e:
         return jsonify(message="An error occurred while processing your request."), 500
 
 @auth.route('/logout', methods=['POST'])
-@jwt_required
+@jwt_required()
 def logout():
     try:
         response = jsonify()
@@ -73,15 +72,3 @@ def logout():
         return response, 200
     except Exception as e:
         return jsonify(message="An error occurred while processing your request."), 500
-
-# @bp.route('/refresh', methods=('POST',))
-# @jwt_refresh_token_required
-# def refresh():
-#   user_id = get_jwt_identity()
-#   user = User.query.filter_by(user_id=user_id).first()
-#   access_token = create_access_token(identity=user.user_id)
-
-#   response = jsonify()
-#   set_access_cookies(response, access_token)
-
-#   return response, 201
