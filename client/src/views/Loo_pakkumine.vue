0a0ee862add324ad4286afdd58742d080c7626c0
ppavli <ppavli@taltech.ee>
2023-11-05 20:10:20 +0200

    Added the option to add files on the Loo_pakkumine page But it is not working properly

--- client/src/views/Loo_pakkumine.vue
+++ client/src/views/Loo_pakkumine.vue
@@ -1,74 +1,88 @@
 <template>
-  <Navbar />
-  <div class="background">
-  <div class="text-4xl font-bold mb-8 p-4 ml-28 background">Loo pakkumine</div>
-  <div class="flex flex-col items-center justify-center background">
-    <div class="bg-white p-8 rounded shadow-2xl w-5/6 h-4/5 min-h-full">
-      <div class="text-xl">
-        <input
-          v-model="productTitle"
-          placeholder="Kirjuta toote tiitel"
-          class="transition ease-in-out delay-150 w-full p-2 border-b-2 border-black focus:outline-none focus:border-indigo-300 duration-300"
-        />
+  <div>
+    <Navbar />
+    <div class="background">
+      <div class="text-4xl font-bold mb-8 p-4 ml-28 background">Loo pakkumine</div>
+      <div class="flex flex-col items-center justify-center background">
+        <div class="bg-white p-8 rounded shadow-2xl w-5/6 h-4/5 min-h-full">
+          <div class="text-xl">
+            <input
+              v-model="productTitle"
+              placeholder="Kirjuta toote tiitel"
+              class="transition ease-in-out delay-150 w-full p-2 border-b-2 border-black focus:outline-none focus:border-indigo-300 duration-300"
+            />
+          </div>
+        </div>
+        <div class="p-4"></div>
+        <div class="flex bg-white rounded shadow-2xl w-5/6 h-4/5 min-h-full">
+          <button
+            :class="{ 'button-active': selectedButton === 'kirjeldus' }"
+            @click="openWindow('kirjeldus')"
+            class="flex-grow button-background text-xl p-8 text-white w-full"
+          >
+            Kirjeldus
+          </button>
+          <button
+            :class="{ 'button-active': selectedButton === 'materjalid' }"
+            @click="openWindow('materjalid')"
+            class="flex-grow button-background text-xl p-8 text-white w-full"
+          >
+            Failid
+          </button>
+          <button
+            :class="{ 'button-active': selectedButton === 'tehniline_info' }"
+            @click="openWindow('tehniline_info')"
+            class="flex-grow button-background text-xl p-8 text-white w-full"
+          >
+            Tehniline Info
+          </button>
+        </div>
+        <div class="flex bg-white rounded shadow-2xl w-5/6 h-4/5">
+          <textarea
+            v-if="selectedButton === 'kirjeldus'"
+            v-model="kirjeldus"
+            placeholder="Siia saad lisada kirjelduse"
+            class="w-full h-40 p-2"
+          ></textarea>
+          <div v-if="selectedButton === 'materjalid'" class="w-full h-40 p-2">
+            <div class="w-full h-8 text-gray-400">Siia saad lisada failid</div>
+            <div class="file-list">
+              <div v-for="(file, index) in fileBoxes[currentBoxIndex]" :key="index" class="file-box">
+                <div class="file-box-content">{{ file.name }}</div>
+                <button class="delete-button" @click="deleteFile(index)">Delete</button>
+              </div>
+              <div class="file-box add-file h-28 w-28" @click="openFileInput">
+                +
+                <input
+                  type="file"
+                  style="display: none"
+                  @change="handleFileChange"
+                  ref="fileInput"
+                />
+              </div>
+            </div>
+          </div>
+          <textarea
+            v-if="selectedButton === 'tehniline_info'"
+            v-model="tehnilineInfo"
+            placeholder="Siia saad lisada tehnilise info"
+            class="w-full h-40 p-2"
+          ></textarea>
+        </div>
+        <div class="p-2"></div>
       </div>
-    </div>
-    <div class="p-4"></div>
-    <div class="flex bg-white rounded shadow-2xl w-5/6 h-4/5 min-h-full">
-      <button
-        :class="{ 'button-active': selectedButton === 'kirjeldus' }"
-        @click="openWindow('kirjeldus')"
-        class="flex-grow button-background text-xl p-8 text-white w-full"
-      >
-        Kirjeldus
-      </button>
-      <button
-        :class="{ 'button-active': selectedButton === 'materjalid' }"
-        @click="openWindow('materjalid')"
-        class="flex-grow button-background text-xl p-8 text-white w-full"
-      >
-        Materjalid
-      </button>
-      <button
-        :class="{ 'button-active': selectedButton === 'tehniline_info' }"
-        @click="openWindow('tehniline_info')"
-        class="flex-grow button-background text-xl p-8 text-white w-full"
-      >
-        Tehniline Info
-      </button>
-    </div>
-    <div class="flex bg-white rounded shadow-2xl w-5/6 h-4/5">
-      <textarea
-        v-if="selectedButton === 'kirjeldus'"
-        v-model="kirjeldus"
-        placeholder="Siia saad lisada kirjelduse"
-        class="w-full h-40 p-2"
-      ></textarea>
-      <textarea
-        v-if="selectedButton === 'materjalid'"
-        v-model="materjalid"
-        placeholder="Siia saad lisada materjalide info"
-        class="w-full h-40 p-2"
-      ></textarea>
-      <textarea
-        v-if="selectedButton === 'tehniline_info'"
-        v-model="tehnilineInfo"
-        placeholder="Siia saad lisada tehnilise info"
-        class="w-full h-40 p-2"
-      ></textarea>
-    </div>
-    <div class="p-2"></div>
-    </div>
-    <div class="flex flex-row justify-center">
-    <div class="flex bg-white rounded shadow-2xl w-1/6 h-4/5 mt-4 p-4 ml-32">
-      <div class="flex items-center">
-        <span class="mr-2">Hind:</span>
-        <textarea v-model="price" placeholder='hind' class="ml-2 w-24 h-10 p-2"></textarea>
-        <span class="ml-2">eur/kuus</span>
+      <div class="flex flex-row justify-center">
+        <div class="flex bg-white rounded shadow-2xl w-1/6 h-4/5 mt-4 p-4 ml-32">
+          <div class="flex items-center">
+            <span class="mr-2">Hind:</span>
+            <textarea v-model="price" placeholder="hind" class="ml-2 w-24 h-10 p-2"></textarea>
+            <span class="ml-2">eur/kuus</span>
+          </div>
+        </div>
+        <div class="flex bg-white rounded shadow-2xl w-1/6 h-4/5 mt-4 p-4 ml-auto mr-32">
+          <button class="button-background w-full h-full p-2 ml-2">Loo pakkumine</button>
+        </div>
       </div>
-    </div>
-    <div class="flex bg-white rounded shadow-2xl w-1/6 h-4/5 mt-4 p-4 ml-auto mr-32">
-        <button class="button-background w-full h-full p-2 ml-2">Loo pakkumine</button>
-    </div>
     </div>
   </div>
 </template>
@@ -85,14 +99,26 @@ export default {
       productTitle: "",
       selectedButton: "kirjeldus",
       kirjeldus: "",
-      materjalid: "",
       tehnilineInfo: "",
       price: "",
+      fileBoxes: [[]], // Array of arrays for each file box
+      showFileModal: false,
+      currentBoxIndex: 0, // Index of the currently active box
     };
   },
   methods: {
     openWindow(button) {
       this.selectedButton = button;
+    },
+    openFileInput() {
+      this.$refs.fileInput.click();
+    },
+    handleFileChange(event) {
+      const files = event.target.files;
+      this.fileBoxes[this.currentBoxIndex] = [...files];
+    },
+    deleteFile(index) {
+      this.fileBoxes[this.currentBoxIndex].splice(index, 1);
     },
   },
 };
@@ -115,9 +141,40 @@ export default {
   background-color: #b4bfff;
 }
 
-/* Red border style with smooth transition */
-.border-red-500 {
-  border-color: #b4beef;
-  transition: border-color 0.3s ease-in-out;
+.file-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.file-box {
+  border: 1px solid #ccc;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  padding: 10px;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+}
+
+.file-box-content {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+.delete-button {
+  background-color: #ff6666;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-top: 5px;
+}
+
+.add-file {
+  background-color: #ddd;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
