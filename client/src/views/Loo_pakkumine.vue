ff7d6a6f57f5ec5953cbb3cb74f2ed6aca792332
Markus Veerbaum <mveerb@taltech.ee>
2023-11-06 14:06:35 +0200

    bug fixes

--- client/src/views/Loo_pakkumine.vue
+++ client/src/views/Loo_pakkumine.vue
@@ -115,7 +115,9 @@ export default {
     },
     handleFileChange(event) {
       const files = event.target.files;
-      this.fileBoxes[this.currentBoxIndex] = [...files];
+      console.log(files)
+      this.fileBoxes[this.currentBoxIndex].push(...files);
+      console.log(...this.fileBoxes)
     },
     deleteFile(index) {
       this.fileBoxes[this.currentBoxIndex].splice(index, 1);
