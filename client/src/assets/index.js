0b8396c0a28c71c423017c48b290daf0826d7513
nimens <nimens@taltech.ee>
2023-12-14 21:59:44 +0200

    Added Payment.vue and some icons

---
+++ client/src/assets/index.js
@@ -0,0 +1,9 @@
+import Amex from "@/assets/payment_systems/Amex.svg";
+import DinersClub from "@/assets/payment_systems/DinersClub.svg";
+import Discover from "@/assets/payment_systems/Discover.svg";
+import JCB from "@/assets/payment_systems/JCB.svg";
+import Mastercard from "@/assets/payment_systems/Mastercard.svg";
+import UnionPay from "@/assets/payment_systems/UnionPay.svg";
+import Visa from "@/assets/payment_systems/Visa.svg";
+
+export { Amex, DinersClub, Discover, JCB, Mastercard, UnionPay, Visa };
